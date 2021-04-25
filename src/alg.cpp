6476ade19c2996eb0e0422abef44754ebc3eec80
Van4inov <78842569+Van4inov@users.noreply.github.com>
2021-04-25 22:05:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,9 +5,65 @@
 std::string infx2pstfx(std::string inf) {
   // добавьте сюда нужный код
   return std::string("");
+   TStack<char> firstStack;
+    std::string strochka;
+    for (int i = 0; i < inf.length(); i++) {
+        if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            strochka += inf[i];
+            strochka += ' ';
+        } else if ((inf[i] == '(') || (priority(inf[i]) > priority(firstStack.get())) || (firstStack.isEmpty())) {
+            firstStack.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (!firstStack.isEmpty() && firstStack.get() != '(') {
+                strochka += firstStack.get();
+                strochka += ' ';
+                firstStack.pop();
+            }
+            if (firstStack.get() == '(')
+                firstStack.pop();
+        } else {
+            while ((!firstStack.isEmpty()) && (priority(firstStack.get()) >= priority(inf[i]))) {
+                strochka = firstStack.get();
+                strochka += ' ';
+                firstStack.pop();
+            }
+            firstStack.push(inf[i]);
+        }
+    }
+    while (!firstStack.isEmpty()) {
+        strochka += firstStack.get();
+        strochka += ' ';
+        firstStack.pop();
+    }
+    return strochka;
 }
 
 int eval(std::string pst) {
   // добавьте сюда нужный код
   return 0;
+   TStack<int> secondStack;
+    int output;
+    for (int i = 0; i < pst.length(); i++) {
+        if ((pst[i] >= '0') && (pst[i] <= '9')) {
+            secondStack.push(pst[i] - '0');
+        } else {
+            if (pst[i] != ' ') {
+                int z = secondStack.get();
+                secondStack.pop();
+                int y = secondStack.get();
+                secondStack.pop();
+                if (pst[i] == '-') {
+                    secondStack.push(y - z);
+                } else if (pst[i] == '+') {
+                    secondStack.push(y + z);
+                } else if (pst[i] == '*') {
+                    secondStack.push(y * z);
+                } else {
+                    secondStack.push(y / z);
+                }
+            }
+        }
+    }
+    output = secondStack.get();
+    return output;
 }
